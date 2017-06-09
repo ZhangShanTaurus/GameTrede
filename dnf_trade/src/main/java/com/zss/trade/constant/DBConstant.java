01157537ec3db3ba2673be9d05020dd5e6edbc0d
张山山 <zhangss@fxiaoke.com>
2017-06-09 14:56:02 +0800

    修改程序包名

---
+++ dnf_trade/src/main/java/com/zss/trade/constant/DBConstant.java
@@ -0,0 +1,10 @@
+package com.zss.trade.constant;
+
+/**
+ * 数据库相关常量
+ * Created by Administrator on 2016/11/9.
+ */
+public class DBConstant {
+    /*------大区------*/
+    private static final int AREA_HUABEI = 1;
+}
---
+++ dnf_trade/src/main/java/com/zss/trade/ui/basic/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.zss.trade.ui.basic;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Fragment基类
+ * Created by Administrator on 2016/11/9.
+ */
+public class BaseFragment extends Fragment {
+}
